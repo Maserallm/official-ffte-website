dd801a4d39d5458d174f1ef4d56efa149da27066
Maserall Marcelin <maserallm@knights.ucf.edu>
2020-11-10 15:47:03 -0500

    gatsby dev

---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li>
+        <Link>This is the navigation bar</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Navbar
