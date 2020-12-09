68d562da03e36df9eb1725f2c41b801928061de1
Maserall Marcelin <maserallm@knights.ucf.edu>
2020-12-09 00:37:44 -0500

    maint mode

--- src/components/maintainance_message.js
+++ src/components/maintainance_message.js
@@ -2,10 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const DateStyle = styled.div`
-  h1 {
+  h11 {
     font-family: santos_dumont;
     display: flex;
-    font-size: 150px;
+    font-size: 120px;
     flex-direction: column;
     text-align: center;
     align-items: center;
@@ -13,13 +13,46 @@ const DateStyle = styled.div`
     position: relative;
     top: 275px;
   }
+
+  .container_ {
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  .fwStyle {
+    font-family: santos_dumont;
+    display: grid;
+    grid-template-columns: 1fr;
+    text-align: center;
+    font-size: 150px;
+    padding: 10px;
+    grid-row: 2 / 3;
+  }
+
+  @media (max-width: 703px) {
+    .container_ {
+      margin: auto;
+      height: 50vh;
+    }
+    .header2 {
+      height: 15vh;
+    }
+
+    .fwStyle {
+      font-size: 75px;
+      align-items: center;
+      justify-items: center;
+    }
+  }
 `;
 
 const Maintenece = () => {
   return (
     <>
       <DateStyle>
-        <h1>12/11/20</h1>
+        <div className="container_">
+          <h1 className="fwStyle">12/11/20</h1>
+        </div>
       </DateStyle>
     </>
   );
